fbb60fcdb6d3fc7bb200cf5962113727aec28de5
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2016-07-25 14:51:31 +0200

    Improve the underbig tactic w.r.t the occurrences to be rewritten.
    Also: Move code from underbig to under_big.

--- src/extra_tactics.v
+++ src/extra_tactics.v
@@ -6,17 +6,24 @@ From mathcomp Require Import prime binomial ssralg.
 
 (** [under_big] allows one to apply a given tactic under the bigop
     that correspond to the specified arguments. *)
-Ltac under_big b R idx op I r P F1 tac :=
-  (* erewrite (@eq_bigr R idx op I r P F1 _); (*not robust enough*) *)
-  pattern b;
-  match goal with
-  | [|- ?G b] =>
-    refine (@eq_rect_r _ _ G _ _ (@eq_bigr R idx op I r P F1 _ _));
-    [|let i := fresh "i" in
-      let Hi := fresh "Hi" in
-      move=> i Hi; (tac || fail 5 "Cannot apply" tac); move: i Hi;
-      first reflexivity];
-    cbv beta
+Ltac under_big b tac :=
+  let b' := eval hnf in b in
+  match b' with
+  | @BigOp.bigop ?R ?I ?idx ?r ?f =>
+    match f with
+    | fun i => @BigBody ?R ?I i ?op (@?P i) (@?F1 i) =>
+      (* erewrite (@eq_bigr R idx op I r P F1 _); (*not robust enough*) *)
+      pattern b;
+      match goal with
+      | [|- ?G b] =>
+        refine (@eq_rect_r _ _ G _ b (@eq_bigr R idx op I r P F1 _ _));
+        [|let i := fresh "i" in
+          let Hi := fresh "Hi" in
+          move=> i Hi; (tac || fail 3 "Cannot apply" tac); move: i Hi;
+          first reflexivity];
+        cbv beta
+      end
+    end
   end.
 
 (* BEGIN 3rdparty *)
@@ -34,29 +41,35 @@ Ltac find_pat pat tac :=
 end.
 (* END 3rdparty *)
 
-(** [underbig] allows one to apply a given tactic under the first
-    bigop that matches the specified pattern *)
+(** [underbig] allows one to apply a given tactic under some bigop:
+    if [pat] is a local variable (let-in) that appears in the goal,
+    only the occurrences of [pat] will be rewritten;
+    otherwise the occurrences of the first bigop that matches [pat]
+    will be rewritten. *)
 Tactic Notation "underbig" open_constr(pat) tactic(tac) :=
-  find_pat pat ltac:(fun b =>
-  let b' := eval hnf in b in change b with b';
-  match b' with
-  | @BigOp.bigop ?R ?I ?idx ?r ?f =>
-    match f with
-    | fun i => @BigBody ?R ?I i ?op (@?P i) (@?F1 i) =>
-    under_big b' R idx op I r P F1 tac
-    end
-  end).
+  tryif match goal with [|- context [pat]] => is_var pat end
+  then under_big pat tac
+  else find_pat pat ltac:(fun b => under_big b tac).
 
 (* TODO: underbig b in H ... *)
 
 Lemma test1 (n : nat) (R : ringType) (f1 f2 g : nat -> R) :
-  (\big[+%R/0%R]_(i < n) ((f1 i + f2 i) * g i) =
+  (\big[+%R/0%R]_(i < n) ((f1 i + f2 i) * g i) +
+  \big[+%R/0%R]_(i < n) ((f1 i + f2 i) * g i) =
+  \big[+%R/0%R]_(i < n) ((f1 i + f2 i) * g i) +
   \big[+%R/0%R]_(i < n) (f1 i * g i) + \big[+%R/0%R]_(i < n) (f2 i * g i))%R.
 Proof.
-set b1 := LHS.
+set b1 := {2}(\big[_/_]_(i < n) _).
 Fail underbig b1 rewrite GRing.mulrDr.
-underbig b1 rewrite GRing.mulrDl.
-by rewrite big_split.
+
+underbig b1 rewrite GRing.mulrDl. (* only b1 is rewritten *)
+
+Undo 1. rewrite /b1.
+underbig b1 rewrite GRing.mulrDl. (* 3 occurrences are rewritten *)
+
+rewrite big_split /=.
+by rewrite GRing.addrA.
+Qed.
 
 Lemma test2 (n : nat) (R : fieldType) (f : nat -> R) :
   (forall k : 'I_n, f k != 0%R) ->
