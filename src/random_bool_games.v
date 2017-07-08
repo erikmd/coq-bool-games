8c93175c822ff83692677af7b110d5fa2fe52de4
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2017-07-08 19:32:40 +0200

    Add a corollary (Pr_ex_winA_winB_disj_Bern).

--- src/random_bool_games.v
+++ src/random_bool_games.v
@@ -1656,6 +1656,12 @@ have <- := @Pr_ex_winA_Bern n (1 - p)%Re (@q_0_1 p Hp) (n - k) (Hnkn _ _).
 by underp big j rewrite inE.
 Qed.
 
+Corollary Pr_ex_winA_winB_disj_Bern :
+  Pr P [set F | [exists a : bg_StratA k, winA (game_of F) a] ||
+                [exists b : bg_StratB n k, winB (game_of F) b]] =
+  2 - (1 - p ^ (2 ^ (n - k))) ^ (2 ^ k) - (1 - (1 - p) ^ (2 ^ k)) ^ (2 ^ (n - k)).
+Proof. rewrite Pr_ex_winA_winB_disj Pr_ex_winA_Bern Pr_ex_winB_Bern; ring. Qed.
+
 (** ** Partial Information on the Opponent's Choices *)
 
 Section parameter_s_fixed.
